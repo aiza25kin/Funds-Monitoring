c9a27e6922fb0b41e097a45d0764c3c83f88a393
aiza25kin <68382400+aiza25kin@users.noreply.github.com>
2020-08-13 15:08:01 +0800

    Update bootstrap.php
    bootstrap sample update

--- LifeInsurance/bootstrap.php
+++ LifeInsurance/bootstrap.php
@@ -1,4 +1,9 @@
 <!-- bootstrap styling -->
+
+
+
+
+
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
